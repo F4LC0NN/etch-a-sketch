b2a6fe8017752892a45a4e6a8273dc220abf28b9
F4LC0NN <thomasruben25@gmail.com>
2022-02-24 10:36:50 +0100

    Generate a dynamic grid

--- js/script.js
+++ js/script.js
@@ -10,3 +10,28 @@ const clearButton = document.querySelector('#clear');
 const newGridButton = document.querySelector('#new-grid');
 const inputGridSize = document.createElement('input');
 
+/*
+-------------------------------------------------------
+GENERATE GRID
+-------------------------------------------------------
+*/
+
+// Create a function to generate a dynamic grid
+function generateGrid() {
+  let i = 0;
+  gridSize = inputGridSize.value ** 2;
+  while (i < gridSize) {
+    const square = document.createElement('div');
+    container.style.gridTemplateColumns = `repeat(${inputGridSize.value}, 0fr)`;
+    square.classList.add('square')
+    container.appendChild(square);
+    square.addEventListener('mouseenter', () => {
+      square.style.backgroundColor = "maroon";
+    });
+
+    clearGrid(square);
+    i++;
+  }
+}
+
+generateGrid();
