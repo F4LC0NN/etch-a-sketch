1e6b78710032846719a48efd0f39d793f928c71c
F4LC0NN <thomasruben25@gmail.com>
2022-02-24 10:33:52 +0100

    Create selectors to generate the grid & trigger the buttons

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+-------------------------------------------------------
+SELECTORS
+-------------------------------------------------------
+*/
+
+// Create selectors for the grid & the button functionalities
+const container = document.querySelector('#container');
+const clearButton = document.querySelector('#clear');
+const newGridButton = document.querySelector('#new-grid');
+const inputGridSize = document.createElement('input');
+
